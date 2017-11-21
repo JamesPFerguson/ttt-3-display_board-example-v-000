3ee9d8f906213a936ef8a4d74fd5a07a342a7e13
James Ferguson <ncjamesferguson@gmail.com>
2017-11-21 16:41:05 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,18 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def squareLine {
+  print "   "
+  print "|"
+  print "   "
+  print "|"
+  puts "   "
+}
+def lineLine {
+  puts "-----------"
+}
+def makeBoard {
+  squareLine()
+  lineLine()
+  squareLine()
+  lineLine()
+  squareLine()
+}
